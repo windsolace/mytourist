21800c6818a45312fe5c5cbceed0be272005f127
windsolace <eugene.nwm@gmail.com>
2018-04-27 01:27:48 +0800

    fixed lost profile code.

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 
 @Component({
   selector: 'page-profile',
   templateUrl: 'profile.html'
 })
 export class ProfilePage {
+	profile:any = {};
+	rating:number[] = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+  				public navParams: NavParams) {
 
   }
+
+  ionViewDidEnter(){
+		this.profile = this.navParams.data;
+		for(let i = 0; i < this.profile.rating; i++){
+			this.rating.push(1);
+			if(i > 5) break;
+		}
+	}
 
 }
